8913361a6d3a78636f1d5d064e00ad1fb3f40d83
MvPorcalla <porcallamelvin@gmail.com>
2024-08-18 02:21:43 +0800

    vendor and admin

--- pages/admin/components/sidebar.php
+++ pages/admin/components/sidebar.php
@@ -1,25 +1,25 @@
- <!-- Sidebar -->
- <nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
-    <div class="position-sticky">
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Library</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Favorites</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">History</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Research Hub Forum</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Account Setting</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Log Out</a>
-            </li>
-        </ul>
+<?php
+    // Simulated user data
+    $user_name = "admin melvs";
+?>
+
+
+<nav class="col-md-3 col-lg-3 d-md-block sidebar">
+    <div class="text-center my-3">
+        <h1 class="sidebar-text">Welcome, <?php echo htmlspecialchars($user_name); ?>!</h1>
     </div>
+    <ul class="nav flex-column mx-4">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Research Record</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">User List</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Pending Request</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Settings</a>
+        </li>
+    </ul>
 </nav>
