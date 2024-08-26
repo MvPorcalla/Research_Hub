95164fdcc4401cb4646a0b3f2da5dd2c30758655
MvPorcalla <porcallamelvin@gmail.com>
2024-08-26 18:24:17 +0800

    sample update search

--- pages/admin/searchui.php
+++ pages/admin/searchui.php
@@ -97,6 +97,6 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
 
-    <script src="searchfetch_1.js"></script>
+    <script src="searchfetch.js"></script>
 </body>
 </html>
