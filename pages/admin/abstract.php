adf2372b0b2cc4463944001f0bdc0d9343aef838
edmarielle <edmariellebernaldez016@gmail.com>
2024-08-27 23:07:55 +0800

    minor changes

--- pages/admin/abstract.php
+++ pages/admin/abstract.php
@@ -9,7 +9,7 @@ if ($_SESSION['user_type'] != 'A') header("location: ../../index.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Record - LNHS Research Hub</title>
+    <title id="abstractTitle">Add Record - LNHS Research Hub</title>
     <link rel="icon" href="../../assets/icons/LNHS-icon.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../../css/mainStyle.css">
--- pages/user/forum.php
+++ pages/user/forum.php
@@ -67,7 +67,7 @@ $forum_entries = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HOME PAGE - LNHS Research Hub</title>
+    <title>FORUM - LNHS Research Hub</title>
     <link rel="icon" href="../../assets/icons/LNHS-icon.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../../css/mainStyle.css">
