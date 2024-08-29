d8bd65528c8ca41d6481d0eb98263b9dd35f498c
edmarielle <edmariellebernaldez016@gmail.com>
2024-08-30 02:16:00 +0800

    backend: edit permissions

--- pages/admin/listLRN.php
+++ pages/admin/listLRN.php
@@ -1,6 +1,13 @@
 <?php
 include_once "..\..\includes\db.php";
-if ($_SESSION['user_type'] != 'A') header("location: ../../index.php");
+
+if (!isset($_SESSION['user_type'])) {
+    header("location: ../../index.php");
+    exit;
+} elseif ($_SESSION['user_type'] != 'A') {
+    header("location: ../../backend/logout.php");
+    exit;
+}
 ?>
 
 <?php include_once "..\..\includes\db.php"; ?>
