4b9417f0c5e3847eff0a5e60073313c21fc7a746
MvPorcalla <porcallamelvin@gmail.com>
2024-09-06 01:32:07 +0800

    edit profile

---
+++ backend/update_profile copy.php
@@ -0,0 +1,48 @@
+<?php
+include_once "../includes/db.php";
+
+if (isset($_POST['lastName'])) {
+    
+    // Assign form values to variables
+    $lastname = $_POST['lastName'];
+    $firstname = $_POST['firstName'];
+    $mi = $_POST['middleInitial'];
+    $username = $_POST['usernameField'];
+    $email = $_POST['emailField'];
+
+    // Fetch the image directory from the database
+    $sql = "SELECT `user_idpicture_imgdir` FROM `users` WHERE `user_id` = ?";
+    $filter = [$_SESSION['user_id']];
+    $result = query($conn, $sql, $filter);
+
+    $old_imgdir = $result[0]['user_idpicture_imgdir'];
+    $fileext = pathinfo($old_imgdir, PATHINFO_EXTENSION); //[ext]
+
+    $new_filename = "{$lastname}, {$firstname} {$mi}";
+    $new_imgdir = "../uploads/idImages/{$new_filename}.{$fileext}";
+
+    $page_type = ($_SESSION['user_type'] == 'A') ? "admin" : "user";
+
+    // Attempt to rename the file
+    if (!rename($old_imgdir, $new_imgdir)) {
+        // Redirect if the renaming fails
+        header("Location: ../pages/{$page_type}/setting.php?editInfo=failed");
+        exit;
+    }
+
+    // Prepare fields for database insertion
+    $table = "users";
+    $fields = [
+        'user_lastname' => $lastname,
+        'user_firstname' => $firstname,
+        'user_mi' => $mi,
+        'user_username' => $username,
+        'user_emailadd' => $email,
+        'user_idpicture_imgdir' => $new_imgdir
+    ];
+    $filter = ['user_id' => $_SESSION['user_id']];
+
+    $status = update($conn, $table, $fields, $filter) ? 'success' : 'failed';
+    header("Location: ../pages/{$page_type}/setting.php?editInfo={$status}");
+    exit;
+}
