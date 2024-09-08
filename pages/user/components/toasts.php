212a688a9eec4728c85e4110a264a467fd512b52
edmarielle <edmariellebernaldez016@gmail.com>
2024-09-09 01:00:17 +0800

    abstract toast

--- pages/user/components/toasts.php
+++ pages/user/components/toasts.php
@@ -1,26 +1,12 @@
-<?php
-    // Sample data for a single notification
-    $notification = [
-        'count' => 23,
-        'time' => '11 mins ago.',
-    ];
-?>
-
 <!-- Toast Container -->
 <div class="toast-container position-fixed bottom-0 end-0 p-3">
-  <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-bs-autohide="true">
+  <div id="liveToast" class="toast border border-dark" role="alert" aria-live="assertive" aria-atomic="true" data-bs-autohide="false">
     <div class="toast-header text-light bg-dark">
-      <span id="abstractCount" class="badge rounded-pill bg-danger me-2">
-        <?php echo htmlspecialchars($notification['count']); ?>
-      </span>
+      <span id="abstractCount" class="badge rounded-pill bg-danger me-2"></span>
       <strong class="me-auto">New abstract</strong>
-      <small>
-        <?php echo htmlspecialchars($notification['time']); ?>
-      </small>
+      <small id="time"></small>
       <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>
-    <div class="toast-body">
-      A <span class="fw-bold text-danger">new abstract has been uploaded</span>. Please review it at your earliest convenience.
-    </div>
+    <div id="toastBody" class="toast-body"></div>
   </div>
 </div>
--- scripts/fetchNotifications.js
+++ scripts/fetchNotifications.js
@@ -15,17 +15,7 @@ async function fetchNotifications() {
             const notificationsContainer = document.getElementById('notificationsContainer');
 
             const latest = timeAgo(notification.latest);
-            
-            if (result.totalCommentCount === 0) {
-                notifHTML = `
-                    <li>
-                        <p class="text-center mb-0">No notifications.</p>
-                    </li>
-                `;
-                
-                if (notificationsContainer) notificationsContainer.innerHTML = notifHTML;
 
-            } else {
                 switch (notification.type) {
                     case 'pending':
     
@@ -47,44 +37,70 @@ async function fetchNotifications() {
                         break;
                 
                     case 'abstract':
-    
-                        // ${notification.count}
-                        // ${notification.latest}
-                        // ${latest} // timeAgo (eg. 11 minutes ago)
+                        
+                        const liveToast = document.getElementById('liveToast');
+                        
+                        if (liveToast && notification.count != 0) {
+                            const abstractCount = liveToast.querySelector('#abstractCount');
+                            abstractCount.innerText = notification.count;
+
+                            const time = liveToast.querySelector('#time');
+                            time.innerText = latest;
+
+                            const toastBody = liveToast.querySelector('#toastBody');
+                            toastBody.innerHTML = (notification.count == 1)
+                                ? `A <span class="fw-bold text-danger">new abstract has been uploaded</span>. Please review them at your convenience.`
+                                : `${notification.count} <span class="fw-bold text-danger"> new abstracts has been uploaded</span>. Please review them at your convenience.`;
+
+                            var toast = new bootstrap.Toast(liveToast);
+                            toast.show();
+                        }
                         
                         break;
                 
                     case 'comment':
     
                         if (notificationsContainer) {
+
+                            if (result.totalCommentCount === 0) {
+                                
+                                notifHTML = `
+                                    <li>
+                                        <p class="text-center mb-0">No notifications.</p>
+                                    </li>
+                                `;
+                                notificationsContainer.innerHTML = notifHTML;
+
+                            } else {
     
-                            const notifHTML = `
-                                <a class="dropdown-item d-flex justify-content-between align-items-center" href="forum.php?entry_id=${notification.entryId}">
-                                    <div class="d-flex flex-column w-100">
-                                        <div class="d-flex justify-content-between align-items-center">
-                                            <div>
-                                                New comment(s) on your entry: "${notification.entryContent}"
-                                                <div class="text-muted">
-                                                    <small>${latest}</small>
+                                const notifHTML = `
+                                    <a class="dropdown-item d-flex justify-content-between align-items-center" href="forum.php?entry_id=${notification.entryId}">
+                                        <div class="d-flex flex-column w-100">
+                                            <div class="d-flex justify-content-between align-items-center">
+                                                <div>
+                                                    New comment(s) on your entry: "${notification.entryContent}"
+                                                    <div class="text-muted">
+                                                        <small>${latest}</small>
+                                                    </div>
                                                 </div>
+                                                <span class="badge ms-5 bg-success rounded-pill">
+                                                    ${notification.count}
+                                                </span>
                                             </div>
-                                            <span class="badge ms-5 bg-success rounded-pill">
-                                                ${notification.count}
-                                            </span>
                                         </div>
-                                    </div>
-                                </a>
-                            `;
-    
-                            const li = document.createElement('li');
-                            li.innerHTML = notifHTML;
-    
-                            notificationsContainer.appendChild(li);
+                                    </a>
+                                `;
+        
+                                const li = document.createElement('li');
+                                li.innerHTML = notifHTML;
+        
+                                notificationsContainer.appendChild(li);
+
+                            }
                         }
                         
                         break;
-                }    
-            }
+                }
                                 
             const notificationCount = document.getElementById('notificationCount');
             if (result.totalCommentCount != 0) notificationCount.innerText = result.totalCommentCount;
