8ad0b7cabb0062b79cba84524f9d85a096baa6d6
edmarielle <edmariellebernaldez016@gmail.com>
2024-09-09 07:24:16 +0800

    no new notif ui

--- scripts/fetchNotifications.js
+++ scripts/fetchNotifications.js
@@ -21,42 +21,53 @@ async function fetchNotifications() {
             const formattedTimePassed = timeAgo(notification.latest);
             const formattedDateTime = formatDateTime(notification.latest);
 
-                switch (notification.type) {
-                    case 'pending':
-                        content = (notification.count == 1)
-                            ? `There's a <span class="fw-bold text-danger">pending request</span>. Please review it at your convenience.`
-                            : `There's <span class="fw-bold text-danger">${notification.count} pending requests</span>. Please review them at your convenience.`;
-
-                        populateToastContent(liveToast, notification.count, content, formattedTimePassed, formattedDateTime)
+            if (result.totalNotifCount === 0) {
+                
+                notifHTML = `
+                    <li>
+                        <p style="width: 350px;" class="text-center mb-0">No new notifications.</p>
+                    </li>
+                `;
+                notificationsContainer.innerHTML = notifHTML;
     
-                        break;
-                
-                    case 'abstract':
-                        
-                        content = (notification.count == 1)
-                            ? `A <span class="fw-bold text-danger">new abstract</span> has been uploaded. Please review it at your convenience.`
-                            : `${notification.count} <span class="fw-bold text-danger"> new abstracts has been uploaded</span>. Please review them at your convenience.`;
+            }
 
-                        populateToastContent(liveToast, notification.count, content, formattedTimePassed, formattedDateTime)
-                        
-                        break;
-                
-                    case 'comment':
+            switch (notification.type) {
+                case 'pending':
+                    content = (notification.count == 1)
+                        ? `There's a <span class="fw-bold text-danger">pending request</span>. Please review it at your convenience.`
+                        : `There's <span class="fw-bold text-danger">${notification.count} pending requests</span>. Please review them at your convenience.`;
 
-                        type = `repl` + ((notification.count == 1) ? 'y' : 'ies');
+                    populateToastContent(liveToast, notification.count, content, formattedTimePassed, formattedDateTime)
 
-                        populateNotificationContent(notificationsContainer, result.totalNotifCount, notification.entryId, notification.entryContent, notification.count, type, formattedDateTime, formattedTimePassed)
-                        
-                        break;
-                
-                    case 'like':
+                    break;
+            
+                case 'abstract':
+                    
+                    content = (notification.count == 1)
+                        ? `A <span class="fw-bold text-danger">new abstract</span> has been uploaded. Please review it at your convenience.`
+                        : `${notification.count} <span class="fw-bold text-danger"> new abstracts has been uploaded</span>. Please review them at your convenience.`;
 
-                        type = `like` + ((notification.count == 1) ? '' : 's');
+                    populateToastContent(liveToast, notification.count, content, formattedTimePassed, formattedDateTime)
+                    
+                    break;
+            
+                case 'comment':
 
-                        populateNotificationContent(notificationsContainer, result.totalNotifCount, notification.entryId, notification.entryContent, notification.count, type, formattedDateTime, formattedTimePassed)
-                        
-                        break;
-                }
+                    type = `repl` + ((notification.count == 1) ? 'y' : 'ies');
+
+                    populateNotificationContent(notificationsContainer, result.totalNotifCount, notification.entryId, notification.entryContent, notification.count, type, formattedDateTime, formattedTimePassed)
+                    
+                    break;
+            
+                case 'like':
+
+                    type = `like` + ((notification.count == 1) ? '' : 's');
+
+                    populateNotificationContent(notificationsContainer, result.totalNotifCount, notification.entryId, notification.entryContent, notification.count, type, formattedDateTime, formattedTimePassed)
+                    
+                    break;
+            }
                                 
             const notificationCount = document.getElementById('notificationCount');
             if (result.totalNotifCount != 0) notificationCount.innerText = result.totalNotifCount;
@@ -96,41 +107,29 @@ function populateToastContent(liveToast, count, content, formattedTimePassed, fo
 function populateNotificationContent(notificationsContainer, totalNotifCount, entryId, entryContent, count, type, formattedDateTime, formattedTimePassed) {
     
     if (notificationsContainer) {
-
-        if (totalNotifCount === 0) {
-            
-            notifHTML = `
-                <li>
-                    <p class="text-center mb-0">No new notifications.</p>
-                </li>
-            `;
-            notificationsContainer.innerHTML = notifHTML;
-
-        } else {
         
-            const notifHTML = `
-                <a class="dropdown-item d-flex justify-content-between align-items-center" href="forum.php?entry_id=${entryId}">
-                    <div class="d-flex flex-column w-100">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="notif-content">
-                                <span class="fs-lg">New ${type} on your entry:</span><br>"<strong>${entryContent}</strong>"
-                                <div class="text-muted">
-                                    <small title="${formattedDateTime}">${formattedTimePassed}</small>
-                                </div>
+        const notifHTML = `
+            <a class="dropdown-item d-flex justify-content-between align-items-center" href="forum.php?entry_id=${entryId}">
+                <div class="d-flex flex-column w-100">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="notif-content">
+                            <span class="fs-lg">New ${type} on your entry:</span><br>"<strong>${entryContent}</strong>"
+                            <div class="text-muted">
+                                <small title="${formattedDateTime}">${formattedTimePassed}</small>
                             </div>
-                            <span class="badge ms-5 bg-success rounded-pill">
-                                ${count}
-                            </span>
                         </div>
+                        <span class="badge ms-5 bg-success rounded-pill">
+                            ${count}
+                        </span>
                     </div>
-                </a>
-            `;
+                </div>
+            </a>
+        `;
 
-            const li = document.createElement('li');
-            li.innerHTML = notifHTML;
+        const li = document.createElement('li');
+        li.innerHTML = notifHTML;
 
-            notificationsContainer.appendChild(li);
-
-        }
+        notificationsContainer.appendChild(li);
+            
     }
 }
